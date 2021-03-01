1154692209bebfa8b934274f011f6a0cd894b302
Jason Campbell <jcryuu@gmail.com>
2021-02-28 18:57:05 -0800

    removed comment and fixed spacing

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,4 @@ Rails.application.routes.draw do
       resources :states
     end
   end
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
